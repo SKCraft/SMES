34adcbe4bf9931e419b103f5b48ac71ca0534360
Machir <jasperhidding@gmail.com>
2014-08-29 18:33:16 +0200

    Tabs to spaces

--- src/main/java/com/skcraft/smes/SMES.java
+++ src/main/java/com/skcraft/smes/SMES.java
@@ -12,27 +12,27 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid=SMESConstants.MOD_ID, name=SMESConstants.NAME)
 public class SMES {
-	@Instance("smes")
-	public static SMES instance;
-	
-	@SidedProxy(clientSide = "com.skcraft.smes.client.ClientProxy", serverSide="com.skcraft.smes.CommonProxy")
-	public static CommonProxy proxy;
-	
-	public static Logger log;
-	
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent evt) {
-		log = evt.getModLog();
-		log.info("Starting SMES...");
-	}
-	
-	@EventHandler
-	public void init(FMLInitializationEvent evt) {
-		
-	}
-	
-	@EventHandler
-	public void postInit(FMLPostInitializationEvent evt) {
-		log.info("SMES initialized");
-	}
+    @Instance("smes")
+    public static SMES instance;
+    
+    @SidedProxy(clientSide = "com.skcraft.smes.client.ClientProxy", serverSide="com.skcraft.smes.CommonProxy")
+    public static CommonProxy proxy;
+    
+    public static Logger log;
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent evt) {
+        log = evt.getModLog();
+        log.info("Starting SMES...");
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent evt) {
+        
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent evt) {
+        log.info("SMES initialized");
+    }
 }
