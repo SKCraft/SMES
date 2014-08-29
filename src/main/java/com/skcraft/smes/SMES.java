38cce4a049b708246ca3edcb353eb466ee137803
Raevox Castles <raevox@gmail.com>
2014-08-29 12:36:49 -0400

    Moved initializaiton phases to CommonProxy

--- src/main/java/com/skcraft/smes/SMES.java
+++ src/main/java/com/skcraft/smes/SMES.java
@@ -1,19 +1,17 @@
 package com.skcraft.smes;
 
-        import com.skcraft.smes.proxy.CommonProxy;
-        import org.apache.logging.log4j.Logger;
+import com.skcraft.smes.proxy.CommonProxy;
+import org.apache.logging.log4j.Logger;
 
-        import cpw.mods.fml.common.Mod;
-        import cpw.mods.fml.common.Mod.EventHandler;
-        import cpw.mods.fml.common.Mod.Instance;
-        import cpw.mods.fml.common.SidedProxy;
-        import cpw.mods.fml.common.event.FMLInitializationEvent;
-        import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-        import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid=SMESConstants.MOD_ID, name=SMESConstants.NAME)
 public class SMES {
-    @Instance("smes")
+    @Mod.Instance("smes")
     public static SMES instance;
 
     @SidedProxy(clientSide = "com.skcraft.smes.proxy.client.ClientProxy", serverSide="com.skcraft.smes.proxy.CommonProxy")
@@ -21,19 +19,17 @@ public class SMES {
 
     public static Logger log;
 
-    @EventHandler
+    @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent evt) {
-        log = evt.getModLog();
-        log.info("Starting SMES...");
+        proxy.preInit(evt);
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent evt) {
-
+        proxy.init(evt);
     }
 
-    @EventHandler
     public void postInit(FMLPostInitializationEvent evt) {
-        log.info("SMES initialized");
+        proxy.postInit(evt);
     }
 }
--- src/main/java/com/skcraft/smes/proxy/CommonProxy.java
+++ src/main/java/com/skcraft/smes/proxy/CommonProxy.java
@@ -1,4 +1,24 @@
 package com.skcraft.smes.proxy;
 
+import com.skcraft.smes.SMES;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
 public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent evt) {
+        SMES.log = evt.getModLog();
+        SMES.log.info("Modulating pre-initialization phase...");
+        SMES.log.info("Pre-initialization phase concluded");
+    }
+
+    public void init(FMLInitializationEvent evt) {
+        SMES.log.info("Modulating initialization phase...");
+        SMES.log.info("Main intializaiton phase concluded");
+    }
+
+    public void postInit(FMLPostInitializationEvent evt) {
+        SMES.log.info("Modulating post-initializaiton phase...");
+        SMES.log.info("Post-initialization phase concluded");
+    }
 }
