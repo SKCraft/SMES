6d2cd702b2f6d92f6575f1445c4fea4b5600d0c2
Machir <jasperhidding@gmail.com>
2014-08-29 22:21:26 +0200

    Decreased the amount of sub items to three and added a generator method for them in ItemPurifiable

--- src/main/java/com/skcraft/smes/item/ItemPurifiable.java
+++ src/main/java/com/skcraft/smes/item/ItemPurifiable.java
@@ -29,94 +29,25 @@ public class ItemPurifiable extends Item implements IToolTipProvider {
     @Override
     @SideOnly(Side.CLIENT)
     public void getSubItems(Item item, CreativeTabs creativeTabs, List list) {
+        // First NBT version with 55.0000000% purity
+        list.add(createSubType(item, 0.550000000f));
+        
+        // Second NBT version with 80.0000000% purity
+        list.add(createSubType(item, 0.800000000f));
+        
+        // Third NBT version with 95.0000000% purity
+        list.add(createSubType(item, 0.950000000f));
+    }
+    
+    private ItemStack createSubType(Item item, float purity) {
         // Prepare itemStack and NBTTagCompound
         NBTTagCompound nbttagcompound = new NBTTagCompound();
         StringBuilder sb = new StringBuilder(SMES.PREFIX).append("purity");
         ItemStack itemStack = new ItemStack(item, 1, 0);
         
-        // First NBT version with 55.0000000% purity
-        nbttagcompound.setDouble(sb.toString(), 0.550000000f);
+        nbttagcompound.setFloat(sb.toString(), purity);
         itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
         
-        // Second NBT version with 70.9677419% purity
-        nbttagcompound.setDouble(sb.toString(), 0.709677419f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Third NBT version with 83.0188679% purity
-        nbttagcompound.setFloat(sb.toString(), 0.830188679f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Fourth NBT version with 90.7216495% purity
-        nbttagcompound.setFloat(sb.toString(), 0.907216495f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Fifth NBT version with 95.1351351% purity
-        nbttagcompound.setFloat(sb.toString(), 0.951351351f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-
-        // Sixth NBT version with 97.5069252% purity
-        nbttagcompound.setFloat(sb.toString(), 0.975069252f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Seventh NBT version with 98.7377279% purity
-        nbttagcompound.setFloat(sb.toString(), 0.987377279f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Eight NBT version with 99.3648553% purity
-        nbttagcompound.setFloat(sb.toString(), 0.993648553f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Ninth NBT version with 99.6814159% purity
-        nbttagcompound.setFloat(sb.toString(), 0.996814159f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Tenth NBT version with 99.8404538% purity
-        nbttagcompound.setFloat(sb.toString(), 0.998404538f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Eleventh NBT version with 99.9201632% purity
-        nbttagcompound.setFloat(sb.toString(), 0.999201632f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Twelfth NBT version with 99.9600657% purity
-        nbttagcompound.setFloat(sb.toString(), 0.999600657f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Thirteenth NBT version with 99.9800288% purity
-        nbttagcompound.setFloat(sb.toString(), 0.999800288f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Fourteenth NBT version with 99.9900134% purity
-        nbttagcompound.setFloat(sb.toString(), 0.999900134f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-
-        // Fifteenth NBT version with 99.9950065% purity
-        nbttagcompound.setFloat(sb.toString(), 0.999950065f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-
-        // Sixteenth NBT version with 99.9975032% purity
-        nbttagcompound.setFloat(sb.toString(), 0.999975032f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
-        
-        // Seventeenth NBT version with 99.9987516% purity
-        nbttagcompound.setFloat(sb.toString(), 0.999987516f);
-        itemStack.setTagCompound(nbttagcompound);
-        list.add(itemStack.copy());
+        return itemStack;
     }
 }
