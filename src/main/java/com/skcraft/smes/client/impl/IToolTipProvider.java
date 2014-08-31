a126c0b0b9b1846f8b859ab9f3a213f98b646f35
Raevox Castles <raevox@gmail.com>
2014-08-31 09:45:22 -0400

    Changed ToolTip provider to be client-sided (and changed method's name)

--- src/main/java/com/skcraft/smes/client/impl/IToolTipProvider.java
+++ src/main/java/com/skcraft/smes/client/impl/IToolTipProvider.java
@@ -1,10 +1,13 @@
 package com.skcraft.smes.client.impl;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
 import java.util.List;
 
 public interface IToolTipProvider {
-    public void provideTooltip(ItemStack itemStack, EntityPlayer player, List<String> toolTip);
+    @SideOnly(Side.CLIENT)
+    public void provideToolTip(ItemStack itemStack, EntityPlayer player, List<String> toolTip);
 }
