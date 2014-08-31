5678f92f9c6fdb52d58b2176803fe9a25eddcf6f
Machir <jasperhidding@gmail.com>
2014-08-31 13:34:12 +0200

    Added SMESGuiHandler and registered it in CommonProxy

---
+++ src/main/java/com/skcraft/smes/client/gui/SMESGuiHandler.java
@@ -0,0 +1,35 @@
+package com.skcraft.smes.client.gui;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+import com.skcraft.smes.tileentity.TileEntityBase;
+
+import cpw.mods.fml.common.network.IGuiHandler;
+
+public class SMESGuiHandler implements IGuiHandler {
+    public static final int MACHINE = 0;
+    
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        if (ID == MACHINE) { 
+            TileEntity tile = world.getTileEntity(x, y, z);
+            if (tile instanceof TileEntityBase) {
+                return ((TileEntityBase) tile).getContainer(player.inventory);
+            }
+        }
+        return null;
+    }
+    
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        if (ID == MACHINE) {
+            TileEntity tile = world.getTileEntity(x, y, z);
+            if (tile instanceof TileEntityBase) {
+                return ((TileEntityBase) tile).getGui(player.inventory);
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/com/skcraft/smes/proxy/CommonProxy.java
+++ src/main/java/com/skcraft/smes/proxy/CommonProxy.java
@@ -1,11 +1,13 @@
 package com.skcraft.smes.proxy;
 
 import com.skcraft.smes.SMES;
+import com.skcraft.smes.client.gui.SMESGuiHandler;
 import com.skcraft.smes.item.SMESItems;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
 
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent evt) {
@@ -25,6 +27,7 @@ public class CommonProxy {
     
     public void registerHandlers() {
         SMES.log.info("Registering handlers...");
+        NetworkRegistry.INSTANCE.registerGuiHandler(SMES.instance, new SMESGuiHandler());
     }
 
     public void postInit(FMLPostInitializationEvent evt) {
